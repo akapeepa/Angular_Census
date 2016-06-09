eba20a6ddbd5d36d855c74c530a207a4dd1ef029
Gurdeep Singh <gurdeep.eec70@gmail.com>
2016-06-09 21:53:17 +0530

    dbs

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,21 @@
 
-var myApp = angular.module('myApp',['ngroute','ngResource']);
+var myApp = angular.module('myApp',['ngRoute','ngResource']);
 
 //Routes
-myApp.config(function($routeProvider))
+myApp.config(function($routeProvider){
+  $routeProvider
+  .when('/',{
+    templateUrl:'pages/home.htm',
+    controller:'MainController',
+    controllerAs:'mc'
+  });
+});
+
+//controller
+myApp.controller('MainController',['$scope','$resource',function($scope,$resource){
+  var vm = this;
+  vm.getCensus = function(){
+    vm.get = $resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP&FIPS='+vm.countrycode+'&time='+vm.year+'&SEX=0&key=b4185ab9ebc26ff683abdbbd02c3c44ded2c9963').query();
+    // console.log(vm.get);
+  }
+}]);
